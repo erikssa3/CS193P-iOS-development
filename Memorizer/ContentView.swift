0dabac396f22d465448808b815c0aeb351508425
Anton Eriksson <anton.eriksson@reaktor.com>
2020-09-06 11:51:55 +0300

    Initial Commit

---
+++ Memorizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorizer
+//
+//  Created by Anton Eriksson on 6.9.2020.
+//  Copyright © 2020 Anton Eriksson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
